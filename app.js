c09c4d5d8556eba66e5ab8e4fe8fe9c144035dfb
EstebanKa <estebankarlenaguirre@gmail.com>
2022-03-14 16:06:52 -0300

    cositas

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const path = require("path");
 
@@ -23,29 +22,3 @@ app.get("/login", (req, res)=>{
 app.get("/register", (req, res)=>{
     res.sendFile(path.join(__dirname, "views/register.html"))
 })
-=======
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-const publicPath = path.resolve(__dirname, "./public")
-
-app.use(express.static(publicPath));
-
-app.listen(process.env.PORT || 3030, () => console.log ("El servidor está funcionando"));
-
-//rutas
-
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "views/home.html"))
-})
-
-app.get("/login", (req, res)=>{
-    res.sendFile(path.join(__dirname, "views/login.html"))
-})
-
-app.get("/register", (req, res)=>{
-    res.sendFile(path.join(__dirname, "views/register.html"))
-})
->>>>>>> 90be41b1e00a948da4852d6938f5f34aca5ebaed
